3cf877d322921d11f73d5014f3b468279368d024
manikanta <sriraj.nb932@gmail.com>
2021-01-04 23:30:17 +0530

    minor changes

--- src/test/java/com/wq/common/Commons.java
+++ src/test/java/com/wq/common/Commons.java
@@ -39,10 +39,11 @@ public class Commons {
 
 		if(browserName.equals("chrome")) {			
 			System.setProperty("webdriver.chrome.driver", prop.getProperty("chromeLocation"));
-//			chromeOptions.addArguments("headless");
-//			chromeOptions.addArguments("--window-size=1920,1080");
-//			driver = new ChromeDriver(chromeOptions);
-			driver = new ChromeDriver();
+			chromeOptions.addArguments("headless");
+			chromeOptions.addArguments("--window-size=1920,1080");
+			chromeOptions.addArguments("--no-sandbox"); //this will bypass OS security model
+			driver = new ChromeDriver(chromeOptions);
+//			driver = new ChromeDriver();
 			driver.manage().window().maximize();
 			driver.get(prop.getProperty(Constants.CUSTOMER_REG_URL));
 		}
